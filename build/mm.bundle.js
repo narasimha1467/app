6bd16e9b9fbb4e07adf99a15050c967cd2360837
narasimha1467 <narasimhadaruru14@gmail.com>
2016-10-20 16:00:28 +0530

    updates

--- build/mm.bundle.js
+++ build/mm.bundle.js
@@ -11817,7 +11817,7 @@ angular.module('mm.addons.mod_flexpaper')
         var presets = {};
         $scope.openurl = function(url){
 	alert(url);
-	window.open(url,'_blank','location=yes'); 
+	window.open(url,'_blank','location=no,toolbar: no'); 
             //var ref = cordova.InAppBrowser.open(url, '_blank', 'location=no,closebuttoncaption=Close,toolbar: no');
             //var ref=window.open(url, '_system', 'location=no,closebuttoncaption=Close,toolbar: no');
 
